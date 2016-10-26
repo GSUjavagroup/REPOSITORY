15618e7121d0855ffad9bf0445a0070080543a26
MatthewJHealy <matthewjhealy@me.com>
2016-10-26 14:28:04 -0400

    .

---
+++ FlightDatabase.java
@@ -0,0 +1,309 @@
+// Creating a public class to store all info for flights.
+public class FlightDatabase {
+
+
+// Alabama	AL
+String AL1 = "Birmingham International Airport	BHM";
+String AL2 = "Dothan Regional Airport	DHM";
+String AL3 = "Huntsville International Airport	HSV";
+String AL4 = "Mobile	MOB";
+String AL5 = "Montgomery	MGM";
+
+
+// Alaska	AK
+String AK1 = "Anchorage International Airport	ANC";
+String AK2 = "Fairbanks International Airport	FAI";
+String AK3 = "Juneau International Airport	JNU";
+
+
+// Arizona	AZ
+String AZ1 = "Flagstaff	FLG";
+String AZ2 = "Phoenix, Phoenix Sky Harbor International Airport	PHX";
+String AZ3 = "Tucson International Airport	TUS";
+String AZ4 = "Yuma International Airport	YUM";
+
+
+// Arkansas	AR
+String AR1 = "Fayetteville	FYV";
+String AR2 = "Little Rock National Airport	LIT";
+
+
+// California	CA
+String CA1 = "Burbank	BUR";
+String CA2 = "Fresno	FAT";
+String CA3 = "Long Beach	LGB";
+String CA4 = "Los Angeles International Airport	LAX";
+String CA5 = "Oakland	OAK";
+String CA6 = "Ontario	ONT";
+String CA7 = "Palm Springs	PSP";
+String CA8 = "Sacramento	SMF";
+String CA9 = "San Diego	SAN";
+String CA10 = "San Francisco International Airport	SFO";
+String CA11 = "San Jose	SJC";
+String CA12 = "Santa Ana	SNA";
+
+
+// Colorado	CO
+String CO1 = "Aspen	ASE";
+String CO2 = "Colorado Springs	COS";
+String CO3 = "Denver International Airport	DEN";
+String C04 = "Grand Junction	GJT";
+String C05 = "Pueblo	PUB";
+
+
+// Connecticut	CT
+String CT1 = "Hartford	BDL";
+
+
+// District of Columbia	DC
+String DC1 = "Washington, Dulles International Airport	IAD";
+String DC2 = "Washington National Airport	DCA";
+
+
+// Florida	FL
+String FL1 = "Daytona Beach	DAB";
+String FL2 = "Fort Lauderdale-Hollywood International Airport	FLL";
+String FL3 = "Fort Meyers	RSW";
+String FL4 = "Jacksonville	JAX";
+String FL5 = "Key West International Airport	EYW";
+String FL6 = "Miami International Airport	MIA";
+String FL7 = "Orlando	MCO";
+String FL8 = "Pensacola	PNS";
+String FL9 = "St. Petersburg	PIE";
+String FL10 = "Sarasota	SRQ";
+String FL11 = "Tampa	TPA";
+String FL12 = "West Palm Beach	PBI";
+String FL13 = "Panama City-Bay County International Airport	PFN";
+
+
+// Georgia	GA
+String GA1 = "Atlanta Hartsfield International Airport	ATL";
+String GA2 = "Augusta	AGS";
+String GA3 = "Savannah	SAV";
+
+
+// Hawaii	HI
+String HI1 = "Hilo	ITO";
+String HI2 = "Honolulu International Airport	HNL";
+String HI3 = "Kahului	OGG";
+String HI4 = "Kailua	KOA";
+String HI5 = "Lihue	LIH";
+
+
+// Idaho	ID
+String ID1 = "Boise	BOI";
+
+
+// Illinois	IL
+String IL1 = "Chicago Midway Airport	MDW";
+String IL2 = Chicago, O'Hare International Airport Airport	ORD";
+String IL3 = "Moline	MLI";
+String IL4 = "Peoria	PIA";
+
+
+// Indiana	IN
+String IN1 = "Evansville	EVV";
+String IN2 = "Fort Wayne	FWA";
+String IN3 = "Indianapolis International Airport	IND";
+String IN4 = "South Bend	SBN";
+
+
+// Iowa	IA
+String IA1 = "Cedar Rapids	CID";
+String IA Des Moines	DSM
+
+
+// Kansas	KS
+StringWichita	ICT
+
+
+// Kentucky	KY
+StringLexington	LEX
+StringLouisville	SDF
+
+
+// Louisiana	LA
+StringBaton Rouge	BTR
+StringNew Orleans International Airport	MSY
+StringShreveport	SHV
+
+
+// Maine	ME
+StringAugusta	AUG
+StringBangor	BGR
+StringPortland	PWM
+
+
+// Maryland	MD
+StringBaltimore	BWI
+
+
+// Massachusetts	MA
+StringBoston, Logan International Airport	BOS
+StringHyannis	HYA
+StringNantucket	ACK
+StringWorcester	ORH
+
+
+// Michigan	MI
+StringBattlecreek	BTL
+StringDetroit Metropolitan Airport	DTW
+StringDetroit	DET
+StringFlint	FNT
+StringGrand Rapids	GRR
+StringKalamazoo-Battle Creek International Airport	AZO
+StringLansing	LAN
+StringSaginaw	MBS
+
+
+// Minnesota	MN
+StringDuluth	DLH
+StringMinneapolis/St.Paul International Airport	MSP
+StringRochester	RST
+
+
+// Mississippi	MS
+StringGulfport	GPT
+StringJackson	JAN
+
+
+// Missouri	MO
+StringKansas City	MCI
+StringSt Louis, Lambert International Airport	STL
+StringSpringfield	SGF
+
+
+// Montana	MT
+StringBillings	BIL
+
+
+// Nebraska	NE
+StringLincoln	LNK
+StringOmaha	OMA
+
+
+// Nevada	NV
+StringLas Vegas, Las Vegas McCarran International Airport	LAS
+StringReno-Tahoe International Airport	RNO
+
+
+// New Hampshire	NH
+StringManchester	MHT
+
+
+// New Jersey	NJ
+StringAtlantic City International Airport	ACY
+StringNewark International Airport	EWR
+StringTrenton	TTN
+
+
+// New Mexico	NM
+StringAlbuquerque International Airport	ABQ
+StringAlamogordo	ALM
+
+
+// New York	NY
+StringAlbany International Airport	ALB
+StringBuffalo	BUF
+StringIslip	ISP
+StringNew York, John F Kennedy International Airport	JFK
+StringNew York, La Guardia Airport	LGA
+StringNewburgh	SWF
+StringRochester	ROC
+StringSyracuse	SYR
+StringWestchester	HPN
+
+
+North Carolina	NC
+Asheville	AVL
+Charlotte/Douglas International Airport	CLT
+Fayetteville	FAY
+Greensboro	GSO
+Raleigh	RDU
+Winston-Salem	INT
+North Dakota	ND
+Bismark	BIS
+Fargo	FAR
+Ohio	OH
+Akron	CAK
+Cincinnati	CVG
+Cleveland	CLE
+Columbus	CMH
+Dayton	DAY
+Toledo	TOL
+Oklahoma	OK
+Oklahoma City	OKC
+Tulsa	TUL
+Oregon	OR
+Eugene	EUG
+Portland International Airport	PDX
+Portland, Hillsboro Airport	HIO
+Salem	SLE
+Pennsylvania	PA
+Allentown	ABE
+Erie	ERI
+Harrisburg	MDT
+Philadelphia	PHL
+Pittsburgh	PIT
+Scranton	AVP
+Rhode Island	RI
+Providence - T.F. Green Airport	PVD
+South Carolina	SC
+Charleston	CHS
+Columbia	CAE
+Greenville	GSP
+Myrtle Beach	MYR
+South Dakota	SD
+Pierre	PIR
+Rapid City	RAP
+Sioux Falls	FSD
+Tennessee	TN
+Bristol	TRI
+Chattanooga	CHA
+Knoxville	TYS
+Memphis	MEM
+Nashville	BNA
+Texas	TX
+Amarillo	AMA
+Austin Bergstrom International Airport	AUS
+Corpus Christi	CRP
+Dallas Love Field Airport	DAL
+Dallas/Fort Worth International Airport	DFW
+El Paso	ELP
+Houston, William B Hobby Airport	HOU
+Houston, George Bush Intercontinental Airport	IAH
+Lubbock	LBB
+Midland	MAF
+San Antonio International Airport	SAT
+Utah	UT
+Salt Lake City	SLC
+Vermont	VT
+Burlington	BTV
+Montpelier	MPV
+Rutland	RUT
+Virginia	VA
+Dulles	IAD
+Newport News	PHF
+Norfolk	ORF
+Richmond	RIC
+Roanoke	ROA
+Washington	WA
+Pasco, Pasco/Tri-Cities Airport	PSC
+Seattle, Tacoma International Airport	SEA
+Spokane International Airport	GEG
+West Virginia	WV
+Charleston	CRW
+Clarksburg	CKB
+Wisconsin	WI
+Green Bay	GRB
+Madison	MSN
+Milwaukee	MKE
+Wyoming	WY
+Casper	CPR
+Cheyenne	CYS
+Jackson Hole	JAC
+Rock Springs	RKS
+
+
+
+} end FlightDatabase.
---
+++ Registration.java
@@ -0,0 +1,60 @@
+// Creating a registration class.
+public class Registration {
+
+  // creating variable to store customer information.
+  private String firstName;
+  private String lastName;
+  private String address;
+  private int zipCode;
+  //.matches(\\d{5});
+  private char[] state;
+  private String userName;
+  // Should program store user names and alert usre if user name is already taken?
+  private String password;
+  private String email;
+  // check for valid email address.
+  // most common email TLD's: .aero .biz .com .coop .edu .gov .info .int .mil .museum
+  // .name .net .org .pro
+
+  private String SSN;
+  private securityQuestion;
+
+// check first/last name for invalid characters (e.g. numbers, dashes).
+// check address for invalid characters.
+// check that zipcode is only numbers - omit last four digits.
+// Check that US state is valid.
+
+//State abbreviations stored in a String array? or Switch-case or something?
+// AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV
+// NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY
+
+
+
+
+
+} // end registration class.
+
+
+public static void main(String[] args) {
+
+  System.out.println("Please enter your LOGIN information below");
+  System.out.println("If you have n ot yet registered, please click \"REGISTER\" below");
+  System.out.println("First Name:");
+  System.out.println("Last Name:");
+  System.out.println("Address:");
+  System.out.println("Zip Code: ");
+  System.out.println("State:");
+  System.out.println("Username:");
+  System.out.println("Password:");
+  System.out.println("Email Address:");
+  System.out.println("Social Security Number:");
+  System.out.println("Please select one of the Secuirty Questions below: ");
+
+
+  String secQues1 = "What town were you born in?";
+  String secQues2 = "What highschool did you graduate from?";
+  String secQues3 = "What was your favorite pet's name?";
+  String secQues4 = "What is you Mother's maiden name?";
+  String secQues5 = "Who is your favorite artist/singer?";
+
+} // end main.

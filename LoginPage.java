5e011e9a7a52218f86d4da84b1b625f0177a06d9
laurahornor <laurahornor@192.168.0.115>
2016-11-29 23:05:07 -0500

    Laura adding draft for login page using JFrame

---
+++ LoginPage.java
@@ -0,0 +1,19 @@
+import java.awt.Container;
+
+import javax.swing.*;
+
+public class LoginPage extends JFrame {
+	
+	Container c = getContentPane();
+	JPanel PFlightTypes = new JPanel(null);
+	JPanel PLogin = new JPanel(null);
+	JPanel PFlightDetails = new JPanel(null);
+	
+	JTextField TFUserName;
+	JPasswordField TPPassword;
+	
+	JLabel LDomesticFlight = new JLabel("<html><B>Domestic Flights</B></html>");
+	JLabel LInternationalFlight = new JLabel("<html><B>International Flights</B></html>");
+
+
+}

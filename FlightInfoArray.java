983e435ee595741112d1d07871fe952619e03c27
laurahornor <laurahornor@192.168.0.115>
2016-11-29 23:08:35 -0500

    Laura adding flight info as multiDim array - framework for mysql?

---
+++ FlightInfoArray.java
@@ -0,0 +1,10 @@
+
+public class FlightInfoArray {
+	
+	final Object[][] row1 = { { "Atlanta", "Seattle", "$500", "05:30" }, { "Atlanta", "Lams Vegas ", "$385", "13:00" }, { "Atlanta", "Charlotte", "$150 ", "18:00" }};
+	final Object[][] row2 = { { "Seattle", "Atlanta", "$500", "08:30" }, { "Las Vegas", "Atlanta ", "$385", "16:00" }, { "Charlotte", "Atlanta", "$150 ", "21:00" }};
+	final Object[][] row3 = { { "New York City", "Washington DC", "$88", "11:30" }, { "San Diego", "Kansas City ", "$129", "09:00" }, { "Detroit", "Miami", "$650 ", "08:00" }};
+	final Object[][] row4 = { { "Washington DC", "New York City", "$88", "14:30" }, { "Kansas City", "San Diego ", "$129", "12:00" }, { "Miami", "Detroit", "$650 ", "11:00" }};
+
+
+}

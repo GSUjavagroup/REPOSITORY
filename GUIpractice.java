a7a3da3a70227c37062356377209db245eda0b47
Shahzad <Shahzad@10.250.80.233>
2016-12-01 12:27:13 -0500

    Updating GUI code again; still not working right - Shahzad

--- GUIpractice.java
+++ GUIpractice.java
@@ -1,5 +1,6 @@
 public class GUIpractice {
 	//this code doesn't work right now but maybe we can figure out how it works
+	//help!
     public static void main(String[] args) {
         final String text = args[0];
         SwingUtilities.invokeLater(new Runnable() {

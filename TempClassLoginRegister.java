90c6fafa3f71e496efb263b7d681be09a7934a57
laurahornor <laurahornor@192.168.0.118>
2016-11-30 23:14:27 -0500

    Laura created temp class for linking registration & login

---
+++ TempClassLoginRegister.java
@@ -0,0 +1,5 @@
+
+public class TempClassLoginRegister {
+//need to create a way to link the Registration information to the login page
+	//let's use this temporary class to share some ideas on how to do that 
+}

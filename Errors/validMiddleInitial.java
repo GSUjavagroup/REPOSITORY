98ecad034795aaef281a5f8f00cef5daeed0997a
GSUjavagroup <lhornor1@student.gsu.edu>
2016-12-13 01:03:42 -0500

    MiddleInitial (from public static boolean) -Shzd

--- Errors/validMiddleInitial.java
+++ Errors/validMiddleInitial.java
@@ -1,13 +1,26 @@
 package MiddleInitial;
 
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class validMiddleInitial {
 	
-	
-public String getInitial(String fullName) { // Shahzad F Irani
-String[] split = fullName.split(" "); // { “Shahzad”, “F”, “Irani” }
-return split[1];
-	}
+//public String getInitial(String fullName) { // Shahzad F Irani
+//String[] split = fullName.split(" "); // { “Shahzad”, “F”, “Irani” }
+//return split[1];
+	//}
 
+//strCheckInitial
+
+	public static boolean checkInitial(String middleInitial) {
+	
+	if (middleInitial.length() == 1 ){
+		return true;
+	}
+	else {
+JOptionPane.showMessageDialog(null, "invalid middleInitial", "Alert", JOptionPane.ERROR_MESSAGE);
+		return false;
+	}
 }
+}
+

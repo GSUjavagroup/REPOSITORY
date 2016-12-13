62eb4b205e40923d2e4e105e6bbed9e676964e1a
GSUjavagroup <lhornor1@student.gsu.edu>
2016-12-13 01:05:43 -0500

    validate PhoneNumber (final one) - Shahzad

--- Errors/validPhone.java
+++ Errors/validPhone.java
@@ -34,7 +34,7 @@ JOptionPane.showMessageDialog(null, "invalid phoneNum", "Alert", JOptionPane.ERR
 }
 
 for (String currentPhoneNum : input) {
-	JOptionPane.showMessageDialog(null, "valid phoneNum", "Hooray", JOptionPane.INFORMATION_MESSAGE);
+//JOptionPane.showMessageDialog(null, "valid phoneNum", "Hooray", JOptionPane.INFORMATION_MESSAGE);
 	System.out.println("Hooray! valid phoneNum: " + currentPhoneNum);
 }
 
@@ -48,20 +48,7 @@ private static String strPhoneNum() {
 private static boolean validatePhoneNumber(String phoneNo) {
 		
 //validate phone numbers of format "1234567890"
-	if (phoneNo.matches("\\d{10}"))
-	return true;
-
-
-	//validating phone number with -, . or spaces
-	else if(phoneNo.matches("\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{4}"))
-	return true;
-
-	//validating phone number with extension length from 3 to 5
-	else if(phoneNo.matches("\\d{3}-\\d{3}-\\d{4}\\s(x|(ext))\\d{3,5}")) 
-	return true;
-
-	//validating phone number where area code is in braces ()
-	else if(phoneNo.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}")) 
+	if (phoneNo.matches("\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{4}"))
 	return true;
 
 	//return false if nothing matches the input
